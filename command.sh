2635cd688a321309771e01e07ba7120d32ccb379
Rii-C <rrishi535@gmail.com>
2023-05-28 13:33:06 +0530

    command.sh

---
+++ command.sh
@@ -0,0 +1,19 @@
+1. npx create-react-app propeller
+2. cd propeller
+3. git init
+4. git add .
+5. gh repo new
+   .
+   blackbird
+   Rii-C
+   task 1
+   public
+   commit 
+   push
+6. git branch upload_logo
+7. git checkout "upload_logo"
+8. git add .
+9. git commit -m "upload logo"
+10. git push origin upload_logo
+11. git checkout master
+12. git pull origin upload_logo
